61fb9c3eba4e3d4bce0cb2600dd9a040e58dd328
LucasMF1 <lucas.mont.freitas@gmail.com>
2026-04-22 11:48:54 -0300

    feat: add error messages and valid tokens for lexer and parser

---
+++ testes_js_to_python/parser/validos/declaracoes.js
@@ -0,0 +1,9 @@
+x = 1
+x += 10
+nome = "teste"
+flag = True
+nada = None
+break
+pass
+import os
+import sys as sistema
---
+++ testes_js_to_python/parser/validos/expressoes.js
@@ -0,0 +1,15 @@
+x + y
+a * b - c
+x == y
+x != y
+x > y
+x >= y
+not x
+-y
+f(x)
+f(a, b, c)
+obj.attr
+obj[0]
+x and y
+x or y
+(x + y) * z
